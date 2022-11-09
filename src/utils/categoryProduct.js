627d774085c97c5ab205cfd2cd4c3012f8b1eaad
Jun <min2jn99@gmail.com>
2022-11-09 15:41:21 +0700

    update category product

--- src/utils/categoryProduct.js
+++ src/utils/categoryProduct.js
@@ -171,6 +171,174 @@ const categoryProduct = [
             },
         ],
     },
+    {
+        type: "Sạc dự phòng",
+        category: "phukien",
+        detailFields: [
+            {
+                name: "unless",
+                label: "hiệu suất sạc",
+            },
+            {
+                name: "battery_capacity",
+                label: "dung lượng pin",
+            },
+            {
+                name: "battery_charge_time",
+                label: "thời gian sạc đầy pin",
+            },
+            {
+                name: "input",
+                label: "nguồn vào",
+            },
+            {
+                name: "output",
+                label: "nguồn ra",
+            },
+            {
+                name: "technology",
+                label: "công nghệ / tiện ích",
+            },
+            {
+                name: "size",
+                label: "kích thước",
+            },
+            {
+                name: "weight",
+                label: "trọng lượng",
+            },
+            {
+                name: "trademark",
+                label: "thương hiệu",
+            },
+            {
+                name: "made_in",
+                label: "sản xuất tại",
+            },
+            {
+                name: "firm",
+                label: "hãng",
+            },
+        ],
+    },
+    {
+        type: "Tai nghe",
+        category: "phukien",
+        detailFields: [
+            {
+                name: "earphone_time",
+                label: "thời gian tai nghe",
+            },
+            {
+                name: "charging_box_time",
+                label: "thời gian hộp sạc",
+            },
+            {
+                name: "charging_port",
+                label: "cổng sạc",
+            },
+            {
+                name: "audio_technology",
+                label: "công nghệ âm thanh",
+            },
+            {
+                name: "compatible",
+                label: "tương thích",
+            },
+            {
+                name: "utilities",
+                label: "tiện ích",
+            },
+            {
+                name: "connection_support",
+                label: "hỗ trợ kết nối",
+            },
+            {
+                name: "controlled_by",
+                label: "điều khiển bằng",
+            },
+            {
+                name: "firm",
+                label: "hãng",
+            },
+        ],
+    },
+    {
+        type: "Bàn phím",
+        category: "phukien",
+        detailFields: [
+            {
+                name: "compatible",
+                label: "tương thích",
+            },
+            {
+                name: "connect",
+                label: "cách kết nối",
+            },
+            {
+                name: "led_light",
+                label: "đèn LED",
+            },
+            {
+                name: "number_of_keys",
+                label: "số phím",
+            },
+            {
+                name: "trademark",
+                label: "thương hiệu",
+            },
+            {
+                name: "firm",
+                label: "hãng",
+            },
+        ],
+    },
+    {
+        type: "Chuột không dây",
+        category: "phukien",
+        detailFields: [
+            {
+                name: "compatible",
+                label: "tương thích",
+            },
+            {
+                name: "default_resolution",
+                label: "độ phân giải mặc định",
+            },
+            {
+                name: "connect",
+                label: "cách kết nối",
+            },
+            {
+                name: "wireconn",
+                label: "độ dài dây / khoảng cách kết nối",
+            },
+            {
+                name: "led_light",
+                label: "đèn LED",
+            },
+            {
+                name: "battery_type",
+                label: "loại pin",
+            },
+            {
+                name: "weight",
+                label: "khối lượng",
+            },
+            {
+                name: "trademark",
+                label: "thương hiệu",
+            },
+            {
+                name: "made_in",
+                label: "sản xuất tại",
+            },
+            {
+                name: "firm",
+                label: "hãng",
+            },
+        ],
+    },
 ];
 
 export default categoryProduct;
